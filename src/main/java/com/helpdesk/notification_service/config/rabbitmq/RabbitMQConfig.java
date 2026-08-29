52356254f9f1821439e86c45700632339a5fe950
sheldon.carvalho <ratchubadasratchufly@gmail.com>
2026-08-29 00:55:28 -0300

    chore: config of RabbitMQ

--- src/main/java/com/helpdesk/notification_service/config/rabbitmq/RabbitMQConfig.java
+++ src/main/java/com/helpdesk/notification_service/config/rabbitmq/RabbitMQConfig.java
@@ -1,17 +1,43 @@
 package com.helpdesk.notification_service.config.rabbitmq;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.JacksonJsonMessageConverter;
 
 @Configuration
 public class RabbitMQConfig {
-    @Bean("helpdesk.exchange")
-    public TopicExchange topicExchange() {}
+
+    public static final String EXCHANGE_NAME = "helpdesk.exchange";
+    public static final String TICKET_QUEUE = "ticket.queue";
+    public static final String TICKET_ROUTING_KEY = "ticket.*";
 
     @Bean
-    public Queue notificationQueue() {}
+    public TopicExchange topicExchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
 
+    @Bean
+    public Queue ticketQueue() {
+        return QueueBuilder
+                .durable(TICKET_QUEUE)
+                .build();
+    }
+
+    @Bean
+    public Binding ticketBinding(
+            Queue ticketQueue,
+            TopicExchange helpdeskExchange
+    ) {
+        return BindingBuilder
+                .bind(ticketQueue)
+                .to(helpdeskExchange)
+                .with(TICKET_ROUTING_KEY);
+    }
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
 
 }
